09ce181cd8fe3525ffad72aeb0e13dbf57efac6f
Rachel Wanke <rkhorne09@gmail.com>
2021-03-25 11:35:47 -0400

    edits to server file

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-app.use(express.static(path.join(__dirname, 'dist')));
+//app.use(express.static(path.join(__dirname, 'dist')));
 
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/budgettracker',
@@ -24,10 +24,18 @@ mongoose.connect(
     useCreateIndex: true,
     useFindAndModify: false
   }
-);
+).then(() => {
+  console.log('Connected to MongoDB')
+}).catch(error => {
+  console.log(error);
+});
 
 // routes
 app.use(require("./routes/api.js"));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
