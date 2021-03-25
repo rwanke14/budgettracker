84484e88b67b20cbbd8e822d8165718a45e0ce83
Rachel Wanke <rkhorne09@gmail.com>
2021-03-25 11:18:29 -0400

    editing package.json

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/budgettracker',
